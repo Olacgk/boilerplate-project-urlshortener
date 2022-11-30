21f1f1010a7f490dcbf610f6f6861433f6b5c7b2
Theotime <theotime.chognika@luxdatadigit.lu>
2022-11-30 10:40:36 +0100

    projet soumis

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ const app = express();
 const mongoose = require('mongoose');
 const dns = require('dns');
 const bodyParser = require("body-parser");
+const shortid = require('shortid');
 const { Schema } = mongoose;
+const urlparser = require('url');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true})
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true}).then(resolve => {console.log('connected')})
 
 let urlSchema = mongoose.model("urlSchema", new Schema({
   original_url: {type: String, required: true},
-  short_url: {type: String, required: true}
+  short_url: String
 }));
 
 app.use(bodyParser.urlencoded({extended: false}))
@@ -31,8 +33,32 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.get('/api/shorturl', function(req, res){
-  
+app.get('/api/shorturl/:short_url', function(req, res){
+  urlSchema.findOne({short_url: req.params.short_url}, function(err, url){
+    if (err) return console.log(err);
+    return res.redirect(url.original_url);
+  })
+});
+
+app.post('/api/shorturl', function(req, res){
+  var original_url = req.body.url
+  var short_url = Date.now()
+  const newUrl = new urlSchema({
+    original_url: original_url,
+    short_url: short_url
+  })
+  const something = dns.lookup(urlparser.parse(original_url).hostname, (error, address) => {
+   if (!address) {
+     res.json({error: 'invalid url'})
+   } else {
+     newUrl.save((err, data) => {
+       res.json({
+         "original_url": newUrl.original_url,
+         "short_url": newUrl.short_url
+       })
+     })
+   }
+ })
 })
 
 app.listen(port, function() {
